beaf550dd75e14ff7c1a50276e975749cf2a0a1c
cyril-philippart <philippart.cyril@gmail.com>
2020-10-13 19:26:23 +0200

    premiere version page d'accueil

---
+++ php/home.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>CYNES</title>
+</head>
+<body>
+    <header>
+    <h1>Cynes</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Savoir-Faire</a></li>
+            <li><a href="#">Réalisations</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    </header>
+    <div>
+        <img src="../img/image-accueil-CYNES.jpg" alt="image representant les dunes de sable">
+        <h2>Creative agency</h2>
+    </div>
+    <main>
+        <section>
+            <p>cynes est une agence de communication créative située à Grasse, qui partage les convictions de Monsieur Albert Camus “Créer c’est vivre deux fois.” </p>
+            <p>Créée en 2019 à Grasse, cynes accompagne les professionnels dans la création et le développement de leur image et de leur notoriété. </p>
+            <p>Pour cynes, concevoir une identité visuelle, c’est traduire une personnalité et un univers, puis le retranscrire graphiquement avec justesse et savoir-faire.</p>
+            <p>Passionnée par son métier, cynes aime les belles typographies, les belles photographies, les designs épurés, les vidéos de qualité et les stratégies bien pensées. </p>
+            <p>Et parce que la communication n’est pas réservée uniquement au gros budgets, cynes imagine, conçoit et propose des créations sur-mesure pour vos projets.</p> 
+            <img src="../img/image-cynes-citation.png" alt="image d'un visage de femme avec une sitation">
+        </section>
+        <section>
+            <h1>Savoir-faire</h2>
+            <article>
+                <img src="../img/logo-Cynes-article.png" alt="logo cynes avec une main terminant d'ecrire le slogan de la marque">
+                <h3>Identité visuelle</h3>
+                <q>"le design est l'ambassadeur silencieux de votre marque"</q><strong>Paul Rand</strong>
+                <p>Pour cynes, concevoir votre identité visuelle, c’est traduire votre personnalité et votre univers, puis le retranscrire graphiquement avec justesse et savoir-faire. Votre identité visuelle est imaginée depuis votre logotype jusqu’à votre charte graphique.</p>    
+            </article>
+            <article>
+                <img src="../img/Impressions-et-prints.png" alt="cartes de visites et cartons d'invitations">
+                <h3>Creation Prints</h3>
+                <q>"Tout ce qui être imaginé est réel"</q><strong>Pablo Picasso</strong>
+                <p>Trouver le bon équilibre entre les formes, les couleurs et les matières pour la création de vos supports papiers : cartes de visite, flyers, affiches, catalogues,  dépliants… tel est le métier de cynes pour vous démarquer. </p>
+            </article>
+            <article>
+                <img src="../img/contenu-web-article.png" alt="exemple de post instagram">
+                <h3>Presence digitale</h3>
+                <q>Une image vaut mieux que mille mots</q><strong>Confusins</strong>
+                <p>cynes vous accompagne dans l’élaboration de votre stratégie de Brand Content, et crée pour vous vos contenus visuels à destination de vos communautés sur les réseaux sociaux et pages web. </p>
+            </article>
+        </section>
+    </main>
+    <footer>    
+        <h4>Cynes</h4>
+        <a href="tel:+33669567444">Tél : 06 69 56 74 44</a>
+        <a href="mailto:contact@cynes.fr"> Mail : contact@cynes.fr</a>
+        <a href="#">Instagram</a>
+        <p>2019 CYNES&copy; all rights reserved</p>
+        <a href="#">Mentions légales</a>
+    </footer>
+</body>
+</html>
