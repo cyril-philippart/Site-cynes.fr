0c4d6e55f085b9fb69e44a4aecad009257a652eb
cyril-philippart <philippart.cyril@gmail.com>
2020-11-02 00:45:57 +0100

    creation menu burger sans boostrap

--- php/home.php
+++ php/home.php
@@ -5,42 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="/Site-cynes.fr/css/style.css">
+   <link rel="stylesheet" href="/Site-cynes.fr/css/reset.css">
+    <link rel="stylesheet" 
+    href="/Site-cynes.fr/css/style.css">
     <title>CYNES</title>
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-fluid navbar-light">
-        <h1><a href="#">Cynes</a></h1>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Savoir-Faire</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Philosophie</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Réalisations</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
+    <div class="menu-wrap">
+        <input type="checkbox" class="toggler">
+        <div class="hamburger">
+            <div></div>
         </div>
-    </nav>
-    <div class="main-img">
-        <img src="/Site-cynes.fr/img/image-accueil-CYNES.jpg" class="img-fluid" alt="Responsive image">
+        <div class="menu">
+            <div>
+                <div>
+                    <ul>
+                        <li><a href="#">Savoir-faire</a></li>
+                        <li><a href="#">Philosophie</a></li>
+                        <li><a href="#">Réalisations</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-
-
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 
 </html>
