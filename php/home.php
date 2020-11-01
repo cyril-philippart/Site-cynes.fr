c4720270d594dfd2c38a8289cc7e17c736210a62
cyril-philippart <philippart.cyril@gmail.com>
2020-11-01 20:01:18 +0100

    refonte homepage bootstrap (header + img)

--- php/home.php
+++ php/home.php
@@ -4,68 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="/Site-cynes.fr/css/style.css">
     <title>CYNES</title>
 </head>
 
 <body>
-    <div class="container">
-        <header>
-            <h1><strong>Cynes</strong></h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Savoir-Faire</a></li>
-                    <li><a href="#">Philosophie</a></li>
-                    <li><a href="#">Réalisations</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div>
-            <img src="../img/image-accueil-CYNES.jpg" alt="image representant les dunes de sable">
-            <h2>Creative agency</h2>
+
+    <nav class="navbar navbar-expand-fluid navbar-light">
+        <h1><a href="#">Cynes</a></h1>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Savoir-Faire</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Philosophie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Réalisations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
         </div>
-        <main>
-            <section>
-                <p><strong>cynes</strong> est une agence de communication créative située à Grasse, qui partage les convictions de Monsieur Albert Camus “Créer c’est vivre deux fois.” </p>
-                <p>Créée en 2019 à Grasse, <strong>cynes</strong> accompagne les professionnels dans la création et le développement de leur image et de leur notoriété. </p>
-                <p>Pour <strong>cynes</strong>, concevoir une identité visuelle, c’est traduire une personnalité et un univers, puis le retranscrire graphiquement avec justesse et savoir-faire.</p>
-                <p>Passionnée par son métier, <strong>cynes</strong> aime les belles typographies, les belles photographies, les designs épurés, les vidéos de qualité et les stratégies bien pensées. </p>
-                <p>Et parce que la communication n’est pas réservée uniquement au gros budgets, <strong>cynes</strong> imagine, conçoit et propose des créations sur-mesure pour vos projets.</p>
-                <img src="../img/image-cynes-citation.png" alt="image d'un visage de femme avec une sitation">
-            </section>
-            <section>
-                <h2>Savoir-faire</h2>
-                <article>
-                    <img src="../img/logo-Cynes-article.png" alt="logo cynes avec une main terminant d'ecrire le slogan de la marque">
-                    <h3>Identité visuelle</h3>
-                    <q>"le design est l'ambassadeur silencieux de votre marque"</q><strong>Paul Rand</strong>
-                    <p>Pour <strong>cynes</strong>, concevoir votre identité visuelle, c’est traduire votre personnalité et votre univers, puis le retranscrire graphiquement avec justesse et savoir-faire. Votre identité visuelle est imaginée depuis votre logotype jusqu’à votre charte graphique.</p>
-                </article>
-                <article>
-                    <img src="../img/Impressions-et-prints.png" alt="cartes de visites et cartons d'invitations">
-                    <h3>Creation Prints</h3>
-                    <q>"Tout ce qui être imaginé est réel"</q><strong>Pablo Picasso</strong>
-                    <p>Trouver le bon équilibre entre les formes, les couleurs et les matières pour la création de vos supports papiers : cartes de visite, flyers, affiches, catalogues, dépliants… tel est le métier de <strong>cynes</strong> pour vous démarquer. </p>
-                </article>
-                <article>
-                    <img src="../img/contenu-web-article.png" alt="exemple de post instagram">
-                    <h3>Presence digitale</h3>
-                    <q>Une image vaut mieux que mille mots</q><strong>Confusins</strong>
-                    <p><strong>cynes</strong> vous accompagne dans l’élaboration de votre stratégie de Brand Content, et crée pour vous vos contenus visuels à destination de vos communautés sur les réseaux sociaux et pages web. </p>
-                </article>
-            </section>
-        </main>
-        <footer>
-            <h4><strong>Cynes</strong></h4>
-            <p>Tél : <a href="tel:+33669567444">06 69 56 74 44</a></p>
-            <p>Mail : <a href="mailto:contact@cynes.fr">contact@cynes.fr</a></p>
-            <a href="#">Instagram</a>
-            <p>2019 <strong>CYNES</strong>&copy; all rights reserved</p>
-            <a href="#">Mentions légales</a>
-        </footer>
+    </nav>
+    <div class="main-img">
+        <img src="/Site-cynes.fr/img/image-accueil-CYNES.jpg" class="img-fluid" alt="Responsive image">
     </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 
 </html>
