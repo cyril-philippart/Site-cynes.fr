1961d161a5e7a2ed4de566ab2c608d12b2f3e251
cyril-philippart <philippart.cyril@gmail.com>
2020-11-02 19:49:29 +0100

    stylisation ouverture menu Burger

--- php/home.php
+++ php/home.php
@@ -5,31 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-   <link rel="stylesheet" href="/Site-cynes.fr/css/reset.css">
-    <link rel="stylesheet" 
-    href="/Site-cynes.fr/css/style.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>CYNES</title>
 </head>
 
 <body>
-    <div class="menu-wrap">
-        <input type="checkbox" class="toggler">
-        <div class="hamburger">
-            <div></div>
-        </div>
-        <div class="menu">
-            <div>
-                <div>
-                    <ul>
-                        <li><a href="#">Savoir-faire</a></li>
-                        <li><a href="#">Philosophie</a></li>
-                        <li><a href="#">Réalisations</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
+    <header>
+        <div class="menu-wrap">
+            <input type="checkbox" class="toggler">
+            <div class="hamburger">
+                <div></div>
             </div>
+            <div class="menu">
+                <div>
+                    <div>
+                        <ul>
+                            <li><a href="#">Savoir-faire</a></li>
+                            <li><a href="#">Philosophie</a></li>
+                            <li><a href="#">Réalisations</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-    </div>
+        <img class="logo-cynes" src="../img/Logo + Baseline.png" alt="Logo Cynes" height="90">
+        <div class="main-image">
+            <img src="../img/image-accueil-CYNES.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+    </header>
+    
+
 </body>
 
 </html>
