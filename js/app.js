0c3fec77b9078c35bde5dbdd3280e68043eaa5a0
cyril-philippart <philippart.cyril@gmail.com>
2020-11-04 17:21:44 +0100

    navbar header fix :)

--- js/app.js
+++ js/app.js
@@ -1,71 +1,73 @@
-(function () {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
+(function() {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
 
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
+	if ( ! window.requestAnimationFrame ) {
+		window.requestAnimationFrame = function(callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+			timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+		};
+	}
 
-    if (!window.cancelAnimationFrame) {
-        window.cancelAnimationFrame = function (id) {
-            clearTimeout(id);
-        };
-    }
+	if ( ! window.cancelAnimationFrame ) {
+		window.cancelAnimationFrame = function(id) {
+			clearTimeout(id);
+		};
+	}
 }());
 
-(function (w, d, undefined) {
-
-    var el_html = d.documentElement,
-        el_body = d.getElementsByTagName('body')[0],
+(function(w,d,undefined){
+ 
+	var el_html = d.documentElement,
+		el_body = d.getElementsByTagName('body')[0],
         header = d.getElementById('header'),
         lastScroll = w.pageYOffset || el_body.scrollTop,
-        menuIsStuck = function (triggerElement, wScrollTop, lastScroll) {
-            var wScrollTop = w.pageYOffset || el_body.scrollTop,
-                regexp = /(nav\-is\-stuck)/i,
-                classFound = el_html.className.match(regexp),
-                navHeight = header.offsetHeight,
-                bodyRect = el_body.getBoundingClientRect(),
-                scrollValue = 600;
-
-            // si le scroll est d'au moins 600 et
-            // la class nav-is-stuck n'existe pas sur HTML
-            if (wScrollTop > scrollValue && !classFound && wScrollTop < lastScroll) {
-                el_html.className = el_html.className + ' nav-is-stuck';
-                el_body.style.paddingTop = navHeight + 'px';
-            }
-
-            // si le scroll est inférieur à 2 et
+		menuIsStuck = function(triggerElement, wScrollTop, lastScroll) {
+			var regexp		= /(nav\-is\-stuck)/i,
+				classFound	= el_html.className.match( regexp ),
+				navHeight	= header.offsetHeight,
+				bodyRect	= el_body.getBoundingClientRect(),
+				scrollValue	= triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight  : 300;
+ 
+			// si le scroll est d'au moins 300 et
+			// la class nav-is-stuck n'existe pas sur HTML
+			if ( wScrollTop > scrollValue && !classFound && wScrollTop < lastScroll) {
+				el_html.className = el_html.className + ' nav-is-stuck';
+				el_body.style.paddingTop = navHeight + 'px';
+			}
+ 
+			// si le scroll à depassé 300 et que l'utilisateur souhaite remonter
             // la class nav-is-stuck existe
-            if (classFound && wScrollTop > lastScroll) {
-                el_html.className = el_html.className.replace(regexp, '');
-                el_body.style.paddingTop = '0';
-            }
+            // le header s'affiche
+			if ( classFound && wScrollTop > lastScroll) {
+				el_html.className = el_html.className.replace( regexp, '' );
+				el_body.style.paddingTop = '0';
+			}
 
-        },
-        onScrolling = function () {
-            // on récupère la valeur du scroll maintenant
-            var wScrollTop = w.pageYOffset || el_body.scrollTop;
-            // on ajoute deux arguments, valeurs de scrolls
-            menuIsStuck(d.getElementById('main'), wScrollTop, lastScroll);
-            // on enregistre notre dernière valeur de scroll
-            lastScroll = wScrollTop;
+		},
+		onScrolling = function() {
+        // on récupère la valeur du scroll maintenant
+        var wScrollTop = w.pageYOffset || el_body.scrollTop;  
+        // on ajoute deux arguments, valeurs de scrolls
+        menuIsStuck( d.getElementById('main-image'), wScrollTop, lastScroll );     
+        // on enregistre notre dernière valeur de scroll
+        lastScroll = wScrollTop;       
         };
-
-    // quand on scroll
-    w.addEventListener('scroll', function () {
-        // on exécute la fonction onScrolling()
-        w.requestAnimationFrame(onScrolling);
-    });
-
+ 
+	// quand on scroll
+	w.addEventListener('scroll', function(){
+		// on exécute la fonction onScrolling()
+		w.requestAnimationFrame( onScrolling );
+	});
+ 
 }(window, document));
+
+
