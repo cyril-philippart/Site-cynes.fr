cbcfb54097a80b469e080a60502d1a0b31dedc5a
cyril-philippart <philippart.cyril@gmail.com>
2020-11-09 00:18:01 +0100

    changement img + orga dossier

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>CYNES</title>
 </head>
 
@@ -30,10 +30,10 @@
                 </div>
             </div>
         </div>
-        <a href="#"><img class="logo-cynes" src="../img/Logo + Baseline.png" alt="Logo Cynes"></a>
+        <a href="#"><img class="logo-cynes" src="./img/Logo + Baseline.png" alt="Logo Cynes"></a>
     </header>
     <div class="main-image">
-        <img src="../img/image-accueil-CYNES.jpg" class="img-fluid" alt="Responsive image">
+        <img src="./img/image-principale.jpg" class="img-fluid" alt="Responsive image">
     </div>
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
@@ -55,7 +55,7 @@
   <div class="card">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Identité visuelle</h5>
+      <h1 class="card-title">Identité visuelle</h1>
       <p class="card-text"><small class="text-muted">"le design est l'ambassadeur silencieux de votre marque" <em>Paul Rand</em></small></p>
       <p class="card-text">Pour cynes, concevoir votre identité visuelle, c’est traduire votre personnalité et votre univers, puis le retranscrire graphiquement avec justesse et savoir-faire. Votre identité visuelle est imaginée depuis votre logotype jusqu’à votre charte graphique.</p>
     </div>
@@ -63,7 +63,7 @@
   <div class="card">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Création design</h5>
+      <h1 class="card-title">Création design</h1>
       <p class="card-text"><small class="text-muted">"Tout ce qui être imaginé est réel"<em> Pablo Picasso</em></small></p>
       <p class="card-text">Trouver le bon équilibre entre les formes, les couleurs et les matières pour la création de vos supports papiers : cartes de visite, flyers, affiches, catalogues,  dépliants… tel est le métier de cynes pour vous démarquer.</p>
     </div>
@@ -71,13 +71,13 @@
   <div class="card">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Digital</h5>
+      <h1 class="card-title">Digital</h1>
       <p class="card-text"><small class="text-muted">"Une image vaut mieux que mille mots"<em> Confusions</em></small></p>
       <p class="card-text">cynes vous accompagne dans l’élaboration de votre stratégie de Brand Content, et crée pour vous vos contenus visuels à destination de vos communautés sur les réseaux sociaux et pages web.</p>
     </div>
   </div>
 </div>
-    <script src="../js/app.js"></script>
+    <script src="./js/app.js"></script>
 </body>
 
 </html>
