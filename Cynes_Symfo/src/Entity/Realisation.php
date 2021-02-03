fbda15d3839fed8ca3528c551653144628e52e53
cyril-philippart <philippart.cyril@gmail.com>
2021-02-03 17:43:25 +0100

    implement Entity for DB

---
+++ Cynes_Symfo/src/Entity/Realisation.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RealisationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RealisationRepository::class)
+ */
+class Realisation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $picture;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Expertise::class)
+     */
+    private $expertise;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Project::class)
+     */
+    private $project;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getExpertise(): ?Expertise
+    {
+        return $this->expertise;
+    }
+
+    public function setExpertise(?Expertise $expertise): self
+    {
+        $this->expertise = $expertise;
+
+        return $this;
+    }
+
+    public function getProject(): ?Project
+    {
+        return $this->project;
+    }
+
+    public function setProject(?Project $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+}
