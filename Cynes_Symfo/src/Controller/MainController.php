7c6470fda5575a55fed68758095a0c6705b35a18
cyril-philippart <philippart.cyril@gmail.com>
2021-02-10 19:44:35 +0100

    display logo with request DB

--- Cynes_Symfo/src/Controller/MainController.php
+++ Cynes_Symfo/src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,10 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(): Response
+    public function homepage(ProjectRepository $projectRepository): Response
     {
         return $this->render('main/homepage.html.twig', [
-            'controller_name' => 'MainController',
+            'all_project' => $projectRepository->findAllProjectLogoDESC(),
         ]);
     }
 
--- Cynes_Symfo/src/Repository/ProjectRepository.php
+++ Cynes_Symfo/src/Repository/ProjectRepository.php
@@ -19,32 +19,11 @@ class ProjectRepository extends ServiceEntityRepository
         parent::__construct($registry, Project::class);
     }
 
-    // /**
-    //  * @return Project[] Returns an array of Project objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findAllProjectLogoDESC()
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
+        return $this->createQueryBuilder('project') 
+            ->orderBy('project.id', 'DESC')
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Project
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
